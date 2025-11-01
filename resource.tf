eb33da0d7099b03d136b6ff4965211f903ee6c6d
Yashwant Singh <13216571+yashu9430singh@users.noreply.github.com>
2025-11-01 10:44:56 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,17 +1,17 @@
 variable "aws_region" {
 description = "AWS region to deploy resources"
 type = string
-dfault = "us-east-1"
+default = "us-east-1"
 }
 
 variable "vpc_cidr" {
-description  = "CIDR block fo rht VPC"
+description  = "CIDR block for VPC"
 type  = string
 default = "10.0.0.0/16"
 }
 
 variable "vpc_name" {
-description  = "Name of the VPC:
+description  = "Name of the VPC"
 type = string
 default = "Terraform-VPC"
 }
